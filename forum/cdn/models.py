ad7746e5754721fb5a34cd84de54d29f79dab6a9
László Károlyi <laszlo@karolyi.hu>
2015-02-22 23:13:48 +0100

    Enlarge orig_src size

--- forum/cdn/models.py
+++ forum/cdn/models.py
@@ -9,7 +9,7 @@ class Image(models.Model):
     comment = models.ManyToManyField(
         'base.Comment', null=None, default=1, verbose_name=_('Found in comment'))
     orig_src = models.URLField(
-        verbose_name=_('Original source'), max_length=256)
+        verbose_name=_('Original source'), max_length=512)
     mime_type = models.CharField(verbose_name=_('Mime type'), max_length=100)
     cdn_path = models.CharField(verbose_name=_('Path in CDN'), max_length=256)
     file_hash = models.CharField(verbose_name=_('File hash'), max_length=200)
