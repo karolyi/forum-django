020e0cbab0ea959c32eeecf1f67516f88b50b64d
László Károlyi <laszlo@karolyi.hu>
2015-02-23 01:20:13 +0100

    Adding unique indexes to hash and cdn path fields

--- forum/cdn/models.py
+++ forum/cdn/models.py
@@ -9,10 +9,12 @@ class Image(models.Model):
     comment = models.ManyToManyField(
         'base.Comment', null=None, default=1, verbose_name=_('Found in comment'))
     orig_src = models.URLField(
-        verbose_name=_('Original source'), max_length=512)
+        verbose_name=_('Original source'), max_length=512, db_index=True)
     mime_type = models.CharField(verbose_name=_('Mime type'), max_length=100)
-    cdn_path = models.CharField(verbose_name=_('Path in CDN'), max_length=256)
-    file_hash = models.CharField(verbose_name=_('File hash'), max_length=200)
+    cdn_path = models.CharField(
+        verbose_name=_('Path in CDN'), max_length=255, unique=True)
+    file_hash = models.CharField(
+        verbose_name=_('File hash'), max_length=200, unique=True)
 
     def __str__(self):
         return self.cdn_path
