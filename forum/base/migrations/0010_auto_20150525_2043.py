50464669f640a1f2515a877f14703febb7386b6f
László Károlyi <laszlo@karolyi.hu>
2015-05-25 22:44:29 +0200

    Remoce auto_now_add until the migration is done

---
+++ forum/base/migrations/0010_auto_20150525_2043.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0009_auto_20150525_2038'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='comment',
+            name='time',
+            field=models.DateTimeField(verbose_name='Commented at'),
+        ),
+        migrations.AlterField(
+            model_name='edit',
+            name='timestamp',
+            field=models.DateTimeField(verbose_name='Edit timestamp'),
+        ),
+    ]
