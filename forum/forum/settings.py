d5bb32479120365167afc6d5182fafcc6c4c82c1
László Károlyi <laszlo@karolyi.hu>
2015-07-04 19:42:18 +0200

    Reorganizing settings

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -10,6 +10,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+# Keep this at the beginning, after module imports
+try:
+    from forum.settings_production import *
+except ImportError as e:
+    pass
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -18,7 +25,10 @@ SECRET_KEY = os.environ.get(
     'FORUM_SECRET_KEY', '(tfz61pc=z34-(m)t#ul3^pf%405xb+$=mwy&ozd-h$kq+^b&p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+try:
+    DEBUG
+except NameError:
+    DEBUG = True
 
 TEMPLATE_DEBUG = True
 
@@ -84,6 +94,65 @@ USE_L10N = True
 
 USE_TZ = True
 
+if DEBUG:
+    # For static serving in development
+    mimetypes.add_type('text/x-scss', '.scss', True)
+    mimetypes.add_type('image/x-sass', '.sass', True)
+    mimetypes.add_type('image/svg+xml', '.svg', True)
+    mimetypes.add_type('image/svg+xml', '.svgz', True)
+
+TMP_DIR = os.path.realpath(os.path.join(BASE_DIR, '..', 'tmp'))
+if not os.path.exists(TMP_DIR):
+    os.mkdir(TMP_DIR)
+
+LOG_DIR = os.path.realpath(os.path.join(BASE_DIR, '..', 'logs'))
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+# Logging setup
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': (
+                '[%(asctime)s] %(levelname)s '
+                '[%(name)s:%(lineno)s] %(message)s'),
+            'datefmt': '%d/%b/%Y %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, 'django-log.txt'),
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'logfile'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
@@ -92,3 +161,9 @@ STATIC_URL = '/static/'
 
 PATH_CDN_ROOT = os.path.join(
     os.path.expanduser('~'), 'Work', 'forum-django-cdn', 'original')
+
+# Keep this at the end
+try:
+    from forum.settings_production import *
+except ImportError as e:
+    pass
