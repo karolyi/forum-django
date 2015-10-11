8b40cbbe5c2229e7b9201c32fa2db888088f3a1e
László Károlyi <laszlo@karolyi.hu>
2015-10-11 13:50:34 +0200

    Forgot to add migration

---
+++ crowdfunding/migrations/0002_project_slug.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crowdfunding', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='project',
+            name='slug',
+            field=django_extensions.db.fields.AutoSlugField(blank=True, populate_from=('name',), verbose_name='Slug', unique=True, editable=False),
+        ),
+    ]
