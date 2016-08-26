ea7a920f76f58318ba8b469ce6b016db7a246866
László Károlyi <laszlo@karolyi.hu>
2016-08-26 11:15:42 +0200

    Change transaction strictness to all tables

--- backend/forum/settings.py
+++ backend/forum/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
-            'sql_mode': 'STRICT_TRANS_TABLES',
+            'sql_mode': 'STRICT_ALL_TABLES',
             'charset': 'utf8mb4'
         }
     }
