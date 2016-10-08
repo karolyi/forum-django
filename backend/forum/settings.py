96ce2431b7ab7fb0ba5c3012e5c490c5c8d095de
László Károlyi <laszlo@karolyi.hu>
2016-10-08 16:45:24 +0200

    Adding debug middleware, result of updating Django

--- backend/forum/settings.py
+++ backend/forum/settings.py
@@ -102,6 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 STATICFILES_FINDERS = (
