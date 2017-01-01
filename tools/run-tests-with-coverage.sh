36f00b14bfa42ce74fe865d76c6a1705aa37b05a
László Károlyi <laszlo@karolyi.hu>
2017-01-01 17:28:52 +0100

    Adjusting travis config

--- tools/run-tests-with-coverage.sh
+++ tools/run-tests-with-coverage.sh
@@ -15,9 +15,13 @@ export PATH=$PATH:/bin/:/usr/bin
 rm -rf htmlcov
 
 coverage run --source 'backend/' backend/manage.py test forum --keepdb -v 2
+EXITCODE_TESTS=$?
+
 coverage html  --omit='*/migrations/*'
 
 # Start the http server inly when we're not on travis
 if [[ -z "$TRAVIS" ]]; then
     python3 -m http.server
+else
+    exit $EXITCODE_TESTS
 fi
