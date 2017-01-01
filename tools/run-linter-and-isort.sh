52d2d6df9aabc2bc11b8f4a3630a4e7e10e3804f
László Károlyi <laszlo@karolyi.hu>
2017-01-01 18:36:56 +0100

    Forgot to add linter check

---
+++ tools/run-linter-and-isort.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+MY_DIR=$(cd $(dirname ${BASH_SOURCE[0]})/..;pwd)
+
+cd $MY_DIR
+
+
+if [[ -e $MY_DIR/venv/bin/activate && -z $VIRTUAL_ENV ]]; then
+    # Only activate virtualenv if it exists and not activated yet
+    source $MY_DIR/venv/bin/activate
+fi
+
+isort -c --skip-glob=node_modules --skip-glob=venv
+EXITCODE_ISORT=$?
+if [[ $EXITCODE_ISORT -ne 0 ]]; then
+    # Isort failed
+    if [[ -z "$TRAVIS" ]]; then
+        echo ISORT FAILED
+    else
+        exit $EXITCODE_ISORT
+    fi
+fi
+
+pycodestyle --exclude='*/migrations/*' backend/
+EXITCODE_LINTER=$?
+if [[ $EXITCODE_LINTER -ne 0 ]]; then
+    # Linter failed
+    if [[ -z "$TRAVIS" ]]; then
+        echo LINTER FAILED
+    else
+        exit $EXITCODE_ISORT
+    fi
+fi
+
