561fded45233a5bd30f1a87af0f579dce56f72c5
László Károlyi <laszlo@karolyi.hu>
2017-01-01 18:09:20 +0100

    Adjusting test runner script

--- tools/run-tests-with-coverage.sh
+++ tools/run-tests-with-coverage.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-if [ -n "$(type -t deactivate)" ] && [ "$(type -t deactivate)" = function ]; then
-    # deactivate the virtualenv if activated
-    deactivate
-fi
-
 MY_DIR=$(cd $(dirname ${BASH_SOURCE[0]})/..;pwd)
 
 cd $MY_DIR
 
-source $MY_DIR/venv/bin/activate
 
-export PATH=$PATH:/bin/:/usr/bin
+if [[ -e $MY_DIR/venv/bin/activate && -z $VIRTUAL_ENV ]]; then
+    # Only activate virtualenv if it exists and not activated yet
+    source $MY_DIR/venv/bin/activate
+fi
+
 rm -rf htmlcov
 
-coverage run --source 'backend/' backend/manage.py test forum --keepdb -v 2
+coverage run  --omit='*/migrations/*' --source 'backend/' backend/manage.py test forum --keepdb -v 2
 EXITCODE_TESTS=$?
 
-coverage html  --omit='*/migrations/*'
+coverage html
 
 # Start the http server inly when we're not on travis
 if [[ -z "$TRAVIS" ]]; then
