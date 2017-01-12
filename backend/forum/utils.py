9428f4b2bf5879516a76d26788e1fb0fdeccb746
László Károlyi <laszlo@karolyi.hu>
2017-01-13 00:19:44 +0100

    Removing unused function

--- backend/forum/utils.py
+++ backend/forum/utils.py
@@ -20,13 +20,3 @@ class DjangoTestRunner(ColourRunnerMixin, DiscoverRunner):
     Colorized test runner for the project:
     """
     pass
-
-
-def _add_widget_class(widget, class_name: str):
-    """
-    Add a class passed in `class_name` to the passed widget.
-    """
-    classes = widget.attrs.get('class', '').split(' ')  # type: list
-    classes.remove('')  # Remove the empty string
-    classes.append(class_name)
-    widget.attrs['class'] = ' '.join(classes)
