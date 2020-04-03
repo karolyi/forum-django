d16ce8ea6d810c6ac6be71c7e806ecdf1c9fad42
László Károlyi <laszlo@karolyi.hu>
2020-04-03 17:14:20 +0200

    Intermediate commit

--- backend/forum/base/views/api.py
+++ backend/forum/base/views/api.py
@@ -95,7 +95,7 @@ def v1_archived_topics_start(request: WSGIRequest) -> HttpResponse:
         request=request, topic_type=TOPIC_TYPE_ARCHIVED, page_id=1, force=True)
     return render(
         request=request,
-        template_name='default/base/topic-archived-start.html',
+        template_name='default/base/render-topic-group-page-inside.html',
         context={
             'topic_list': qs_topics_archived})
 
