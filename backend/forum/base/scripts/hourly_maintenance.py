32835bb35d202c7e050b5ddf1dfaf2b7bbb58735
László Károlyi <laszlo@karolyi.hu>
2020-05-03 21:06:12 +0200

    Add hourly maintenance script

---
+++ backend/forum/base/scripts/hourly_maintenance.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from time import time
+
+from django.conf import settings
+
+from forum.utils.locking import PATH_TEMPLOCK_DIR
+
+
+def _clear_templocks():
+    'Delete old templock files.'
+    one_hour_ago = time() - 60 * 60
+    for path in PATH_TEMPLOCK_DIR.glob(pattern='*'):
+        if path.stat().st_mtime <= one_hour_ago:
+            path.unlink()
+
+
+def _clear_old_converted_cdnfiles():
+    'Delete old CDN files that were a result of a conversion.'
+    one_week_ago = time() - 60 * 60 * 24 * 7
+    for size in settings.CDN['IMAGESIZE']:
+        for path in settings.CDN['PATH_SIZES'][size].rglob(
+                pattern='*'):  # type: Path
+            if path.stat().st_mtime >= one_week_ago:
+                continue
+            if path.is_file() or path.is_symlink():
+                path.unlink()
+                print(f'Removed file/symlink {path}')
+                continue
+            try:
+                path.rmdir()
+                print(f'Removed directory {path}')
+            except OSError as exc:
+                if exc.args[0] != 39:
+                    raise
+
+
+def run():
+    'Run maintenance.'
+    _clear_templocks()
+    _clear_old_converted_cdnfiles()
