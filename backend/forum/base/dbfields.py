56b2f7219fbb81497bc78781d80592f33986f7a3
László Károlyi <laszlo@karolyi.hu>
2020-05-31 21:33:04 +0200

    Add a CharBinaryField for case sensitive text storage

--- backend/forum/base/dbfields.py
+++ backend/forum/base/dbfields.py
@@ -1,7 +1,50 @@
 from django.db.backends.mysql.base import DatabaseWrapper
-from django.db.models.fields import BinaryField
+from django.db.backends.mysql.compiler import SQLUpdateCompiler
+from django.db.models.expressions import Col
+from django.db.models.fields import BinaryField, CharField
 
+DatabaseWrapper.data_types['CharBinaryField'] = 'VARBINARY(%(max_length)s)'
 DatabaseWrapper.data_types['Sha512Field'] = 'BINARY(64)'
+
+
+class CharBinaryField(CharField):
+    """
+    `VARBINARY` stored CharField that has no collation, hence case
+    sensitive.
+    """
+    get_db_prep_value = BinaryField.get_db_prep_value
+
+    def get_internal_type(self) -> str:
+        return 'CharBinaryField'
+
+    def to_python(self, value: str) -> bytes:
+        'Convert to bytes to store in DB.'
+        if isinstance(value, bytes) or value is None:
+            return value
+        return value.encode('utf-8')
+
+    def from_db_value(
+        self, value: bytes, expression: Col, connection: DatabaseWrapper,
+    ) -> str:
+        'Convert back to `str` from the DB stored `bytes`.'
+        return value.decode('utf-8')
+
+    def get_db_prep_value(
+        self, value: str, connection: DatabaseWrapper, prepared: bool = False
+    ):
+        'Get a binary representation from the DB itself for storage.'
+        value = super().get_db_prep_value(
+            value=value, connection=connection, prepared=prepared)
+        if value is not None:
+            return connection.Database.Binary(value)
+        return value
+
+    def get_placeholder(
+        self, value: bytes, compiler: SQLUpdateCompiler,
+        connection: DatabaseWrapper
+    ) -> str:
+        'Note the DB we want to store binary data.'
+        return connection.ops.binary_placeholder_sql(value)
 
 
 class Sha512Field(BinaryField):
