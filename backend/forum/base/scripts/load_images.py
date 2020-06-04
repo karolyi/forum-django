fe52a83c8ba0935f60764c96039e20f8cfa400a4
László Károlyi <laszlo@karolyi.hu>
2020-06-04 22:05:00 +0200

    Updating loader script

--- backend/forum/base/scripts/load_images.py
+++ backend/forum/base/scripts/load_images.py
@@ -6,7 +6,9 @@ from forum.cdn.models import Image
 
 def run():
     all_images = list()
-    for image in Image.objects.all():  # type: Image
+    for idx, image in enumerate(Image.objects.all()):  # type: int, Image
+        if idx % 1000 == 0:
+            print(idx)
         abs_path = \
             settings.CDN['PATH_SIZES']['downloaded'].joinpath(image.cdn_path)
         with PilImage.open(fp=abs_path) as pil_image:
