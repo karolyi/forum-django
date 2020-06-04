19679e87984650a31c02cbf4f708458f9b8db64a
László Károlyi <laszlo@karolyi.hu>
2020-06-04 22:40:41 +0200

    Handling UnidentifiedImageError

--- backend/forum/base/scripts/load_images.py
+++ backend/forum/base/scripts/load_images.py
@@ -1,18 +1,25 @@
 from django.conf import settings
 from PIL import Image as PilImage
+from PIL import UnidentifiedImageError
 
 from forum.cdn.models import Image
 
 
 def run():
     all_images = list()
+    to_delete = set()
     for idx, image in enumerate(Image.objects.all()):  # type: int, Image
         if idx % 1000 == 0:
             print(idx)
         abs_path = \
             settings.CDN['PATH_SIZES']['downloaded'].joinpath(image.cdn_path)
-        with PilImage.open(fp=abs_path) as pil_image:
-            image.width, image.height = pil_image.size
-            all_images.append(image)
+        try:
+            with PilImage.open(fp=abs_path) as pil_image:
+                image.width, image.height = pil_image.size
+                all_images.append(image)
+        except UnidentifiedImageError:
+            to_delete.add(image.pk)
     Image.objects.bulk_update(
         objs=all_images, fields=['width', 'height'], batch_size=500)
+    print('to delete:', to_delete)
+    Image.objects.filter(pk__in=to_delete).delete()
