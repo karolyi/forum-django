08b625d91beb10247e3779325aef80bb05e6759a
László Károlyi <laszlo@karolyi.hu>
2020-06-11 18:23:38 +0200

    Bumping packages, specifying charset on db reload

--- tools/reload-dump.sh
+++ tools/reload-dump.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mysql -uroot -e 'drop database `forum-django`;create database `forum-django`';gunzip -c hondaforum.dump.sql.gz |mysql -uroot forum-django
+mysql -uroot -e 'drop database `forum-django`;create database `forum-django` default character set utf8mb4 default collate utf8mb4_unicode_ci';gunzip -c hondaforum.dump.sql.gz |mysql -uroot forum-django
