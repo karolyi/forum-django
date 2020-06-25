e0b5a995157979e1dcd354c5a814bfa5566c8463
László Károlyi <laszlo@karolyi.hu>
2020-06-25 11:47:56 +0200

    Add syslog message to log rotation

--- tools/uwsgi/rotate-log-prod.sh
+++ tools/uwsgi/rotate-log-prod.sh
@@ -2,4 +2,5 @@
 
 if [ -p /tmp/forum-prod.master-fifo.sock ]; then
     echo l >/tmp/forum-prod.master-fifo.sock
+    logger -t forum-prod Uwsgi FIFO socket rotate command sent.
 fi
--- tools/uwsgi/rotate-log-test.sh
+++ tools/uwsgi/rotate-log-test.sh
@@ -2,4 +2,5 @@
 
 if [ -p /tmp/forum-test.master-fifo.sock ]; then
     echo l >/tmp/forum-test.master-fifo.sock
+    logger -t forum-test Uwsgi FIFO socket rotate command sent.
 fi
