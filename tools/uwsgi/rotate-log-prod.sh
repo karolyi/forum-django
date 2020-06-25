25228b9b078c873289d7e7a33079c48eb48f1648
László Károlyi <laszlo@karolyi.hu>
2020-06-25 12:25:59 +0200

    Restarting running instances until https://github.com/unbit/uwsgi/issues/2192 gets fixed

--- tools/uwsgi/rotate-log-prod.sh
+++ tools/uwsgi/rotate-log-prod.sh
@@ -3,4 +3,5 @@
 if [ -p /tmp/forum-prod.master-fifo.sock ]; then
     echo l >/tmp/forum-prod.master-fifo.sock
     logger -t forum-prod Uwsgi FIFO socket rotate command sent.
+    touch ~hondaforum-prod/project/forum-django/backend/forum/wsgi.py
 fi
--- tools/uwsgi/rotate-log-test.sh
+++ tools/uwsgi/rotate-log-test.sh
@@ -3,4 +3,5 @@
 if [ -p /tmp/forum-test.master-fifo.sock ]; then
     echo l >/tmp/forum-test.master-fifo.sock
     logger -t forum-test Uwsgi FIFO socket rotate command sent.
+    touch ~hondaforum-test/project/forum-django/backend/forum/wsgi.py
 fi
