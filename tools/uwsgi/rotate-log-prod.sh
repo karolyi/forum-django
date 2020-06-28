a498be3123deb836e0108258c493b88c645b2163
László Károlyi <laszlo@karolyi.hu>
2020-06-28 22:06:29 +0200

    Change logging and rotation functionality because of https://github.com/unbit/uwsgi/issues/2192

--- tools/uwsgi/rotate-log-prod.sh
+++ tools/uwsgi/rotate-log-prod.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-if [ -p /tmp/forum-prod.master-fifo.sock ]; then
-    echo l >/tmp/forum-prod.master-fifo.sock
-    logger -t forum-prod Uwsgi FIFO socket rotate command sent.
-    touch ~hondaforum-prod/project/forum-django/backend/forum/wsgi.py
-fi
+# if [ -p /tmp/forum-prod.master-fifo.sock ]; then
+#     echo l >/tmp/forum-prod.master-fifo.sock
+#     logger -t forum-prod Uwsgi FIFO socket rotate command sent.
+#     touch ~hondaforum-prod/project/forum-django/backend/forum/wsgi.py
+# fi
+rm /var/log/uwsgi/forum-prod/uwsgi.log
+ln /var/log/uwsgi/forum-prod/uwsgi.log.REAL /var/log/uwsgi/forum-prod/uwsgi.log
+truncate -s 0 /var/log/uwsgi/forum-prod/uwsgi.log.REAL
--- tools/uwsgi/rotate-log-test.sh
+++ tools/uwsgi/rotate-log-test.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
-if [ -p /tmp/forum-test.master-fifo.sock ]; then
-    echo l >/tmp/forum-test.master-fifo.sock
-    logger -t forum-test Uwsgi FIFO socket rotate command sent.
-    touch ~hondaforum-test/project/forum-django/backend/forum/wsgi.py
-fi
+# if [ -p /tmp/forum-test.master-fifo.sock ]; then
+#     echo l >/tmp/forum-test.master-fifo.sock
+#     logger -t forum-test Uwsgi FIFO socket rotate command sent.
+#     touch ~hondaforum-test/project/forum-django/backend/forum/wsgi.py
+# fi
+rm /var/log/uwsgi/forum-test/uwsgi.log
+ln /var/log/uwsgi/forum-test/uwsgi.log.REAL /var/log/uwsgi/forum-test/uwsgi.log
+truncate -s 0 /var/log/uwsgi/forum-test/uwsgi.log.REAL
+
